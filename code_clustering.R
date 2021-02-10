d56a984bb731dce8048a36d973c94b756b40d8a0
gabrielteotonio <gabriel.teotonio@ubee.in>
2021-02-10 10:46:50 -0300

    feat(monte-carlo): add monte carlo results for clustering performance

--- code_clustering.R
+++ code_clustering.R
@@ -3,20 +3,6 @@
 # Date: 2021/01/29
 
 
-# Data loading -----
-data <- read_csv("data/data_banknote_authentication.txt", col_names = FALSE) %>% 
-  rename("variance" = X1, "skewness" = X2, "curtosis" = X3, "entropy" = X4, "class" = X5) %>% 
-  mutate(class = factor(class, levels = c(0, 1), labels = c("real", "fake"))) 
-
-normalize <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x)))
-}
-
-data <- data %>% 
-  mutate_at(c("variance", "skewness", "curtosis", "entropy"), ~normalize(.))
-
-x <- unname(as.matrix(data[,-5]))
-
 # Aux. functions -----
 kernel_gaussian <- function(x, v) {
   
@@ -52,24 +38,58 @@ metric_weight <- function(l, x, v) {
   
 }
 
+quality_measures <- function(membership, label_prior) {
+  
+  label_posterior <- 
+    t(membership) %>% 
+    as.data.frame() %>% 
+    rename("class_1" = V1, "class_2" = V2) %>% 
+    transmute("class" = ifelse(class_1 > class_2, 0, 1)) %>% 
+    mutate(class = factor(class, levels = c(0, 1)))
+  
+  # F-measure -----
+  conf_mat <- confusionMatrix(label_posterior$class, label_prior$class)
+  precision <- conf_mat$table[1,1] / (conf_mat$table[1,1] + conf_mat$table[1,2])
+  recall <- conf_mat$table[1,1] / (conf_mat$table[1,1] + conf_mat$table[2,1])
+  Fmeasure <- 2 * precision * recall / (precision + recall)
+  
+  # Adjusted Rand index -----
+  Rand_index <- adjustedRandIndex(label_posterior$class, label_prior$class)
+  
+  # Modified partition coefficient -----
+  Vpc <- (1/length(label_prior$class)) * sum(diag(membership %*% t(membership)))
+  Mod_Vpc <- 1 - (2 * (1 - Vpc))
+  
+  # Partition entropy -----
+  Vpe <- -(1/length(label_prior$class)) * sum(diag(membership %*% t(log(membership))))
+  
+  return(list("Fmeasure"= Fmeasure,
+              "Rand_index" = Rand_index,
+              "Mod_Vpc" = Mod_Vpc,
+              "Vpe" = Vpe
+              )
+  )
+  
+}
+
 # Algorithm -----
-vkfcm_k_lp <- function(x, c = 2, m = 1.1, T_limit = 150, error_cond = 10^(-10)) {
+vkfcm_k_lp <- function(x, c = 2, m, T_limit = 150, error_cond = 10^(-10)) {
   
   # Initiate objects -----
   V <- matrix(0L, nrow = c, ncol = ncol(x))  
-  U <- replicate(nrow(x),sample(c(0.5, 0.5)))
+  U <- replicate(nrow(x), sample(c(0.5, 0.5)))
   L <- matrix(1, nrow = c, ncol = ncol(x))
   J <- 10^(10)
   J_aux <- 10^(10)
   t <- 1L
   aux_prod <- c(1, 1)
   aux_sum <- 0
-  error <- 0L
+  error <- 10^(10)
   
   initials_kick <- sample(1:nrow(x), c, replace = FALSE)
   V <- x[initials_kick, ]
   #print(V)
-  while(error > error_cond || t <= T_limit) {
+  while(error > error_cond && t <= T_limit) {
       
     # Update prototype -----
     for (i in 1:c) {
@@ -117,7 +137,7 @@ vkfcm_k_lp <- function(x, c = 2, m = 1.1, T_limit = 150, error_cond = 10^(-10)) 
      }
     }
 
-    error <- J_aux - J
+    error <- abs(J_aux - J)
     t <- t + 1
     aux_prod <- c(1, 1)
     
--- code_simulation_clustering.R
+++ code_simulation_clustering.R
@@ -2,11 +2,14 @@
 # Author: Gabriel Teotonio
 # Date: 2021/01/29
 
+# Packages -----
+library(tidyverse)
+library(mclust)
+
 # Data loading -----
 data <- read_csv("data/data_banknote_authentication.txt", col_names = FALSE) %>% 
   rename("variance" = X1, "skewness" = X2, "curtosis" = X3, "entropy" = X4, "class" = X5) %>% 
-  mutate(class = factor(class, levels = c(0, 1), labels = c("real", "fake"))) %>% 
-  head(20)
+  mutate(class = factor(class, levels = c(0, 1)))
 
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -15,11 +18,12 @@ normalize <- function(x) {
 data <- data %>% 
   mutate_at(c("variance", "skewness", "curtosis", "entropy"), ~normalize(.))
 
+label_prior <- data %>% select("class") %>% as.vector()
 x <- unname(as.matrix(data[,-5]))
 
 # Monte Carlo -----
-nrep <- 4
-params_fuzzy <- c(1.1, 1.6, 2)
+nrep <- 100
+params_fuzzy <- c(1.01, 1.1, 1.6, 2)
 result <- list()
 best_J_objective <- 10^(10)
 best <- list()
@@ -31,6 +35,13 @@ for (params in params_fuzzy) {
     
     result <- vkfcm_k_lp(x, m = params)
     
+    measures <- quality_measures(result$Membership, label_prior)
+    measures$int <- n
+    measures$param <- params
+    write.table(measures, "data/results/measures.csv", sep = ",",
+                col.names = !file.exists("data/results/measures.csv"),
+                append = TRUE, row.names = FALSE)
+    
     if (result$J_objective < best_J_objective) {
       best_J_objective <- result$J_objective
       best <- result
@@ -39,7 +50,9 @@ for (params in params_fuzzy) {
   
   best_register[[i]] <- best
   i <- i + 1
+  best_J_objective <- 10^(10)
+  best <- list()
   
 }
 
-best_register[[1]]$Membership
+quality_measures(best_register[[2]]$Membership, label_prior)
